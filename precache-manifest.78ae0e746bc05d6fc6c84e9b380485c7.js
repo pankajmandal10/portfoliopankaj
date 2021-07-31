74a41760af3a83a1378c687ca88e8392a789469e
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-08-01 00:03:16 +0530

    Updates

---
+++ precache-manifest.78ae0e746bc05d6fc6c84e9b380485c7.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32797fb1e4e697774262822d1d4ab27",
+    "url": "/portfoliopankaj/index.html"
+  },
+  {
+    "revision": "a89fb3350679957e11ac",
+    "url": "/portfoliopankaj/static/css/main.872caff8.chunk.css"
+  },
+  {
+    "revision": "198ce65ea206e212c333",
+    "url": "/portfoliopankaj/static/js/2.fd83c2f2.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/portfoliopankaj/static/js/2.fd83c2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89fb3350679957e11ac",
+    "url": "/portfoliopankaj/static/js/main.cc38b8ab.chunk.js"
+  },
+  {
+    "revision": "6c31acf8fc3396e4494d",
+    "url": "/portfoliopankaj/static/js/runtime-main.fc399830.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfoliopankaj/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portfoliopankaj/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/portfoliopankaj/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portfoliopankaj/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portfoliopankaj/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/portfoliopankaj/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/portfoliopankaj/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfoliopankaj/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3b021f8ffcf9f323ea72abc28382e234",
+    "url": "/portfoliopankaj/static/media/address_image.3b021f8f.svg"
+  },
+  {
+    "revision": "871b519c05149abbe11b86d9386165ae",
+    "url": "/portfoliopankaj/static/media/android-icon-36x36.871b519c.png"
+  },
+  {
+    "revision": "e5c4ba08c1a82ef1dc0c72885b61ca05",
+    "url": "/portfoliopankaj/static/media/android.e5c4ba08.svg"
+  },
+  {
+    "revision": "52e0714e5ea7e3b96f50dbfb62de21d1",
+    "url": "/portfoliopankaj/static/media/animated_logo.52e0714e.svg"
+  },
+  {
+    "revision": "a69e1b3418b00f8188170b69a69a148b",
+    "url": "/portfoliopankaj/static/media/aust.a69e1b34.png"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/portfoliopankaj/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "dfcb7a585547c0b125d208b980d6f57f",
+    "url": "/portfoliopankaj/static/media/cloud_infrastructure.dfcb7a58.svg"
+  },
+  {
+    "revision": "b3685be8bafcd5fe4786b39cba69a561",
+    "url": "/portfoliopankaj/static/media/codeInLogo.b3685be8.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfoliopankaj/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/portfoliopankaj/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "/portfoliopankaj/static/media/deeplearning_ai_logo.7a9b2eb0.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfoliopankaj/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "37d9f73310bfa9cdeb361ec758f428c3",
+    "url": "/portfoliopankaj/static/media/dsc_logo.37d9f733.jpg"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/portfoliopankaj/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/portfoliopankaj/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portfoliopankaj/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portfoliopankaj/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/portfoliopankaj/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portfoliopankaj/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portfoliopankaj/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portfoliopankaj/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/portfoliopankaj/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portfoliopankaj/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfoliopankaj/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfoliopankaj/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portfoliopankaj/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portfoliopankaj/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portfoliopankaj/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfoliopankaj/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/portfoliopankaj/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "871b519c05149abbe11b86d9386165ae",
+    "url": "/portfoliopankaj/static/media/favicon-16x16.871b519c.png"
+  },
+  {
+    "revision": "871b519c05149abbe11b86d9386165ae",
+    "url": "/portfoliopankaj/static/media/favicon-32x32.871b519c.png"
+  },
+  {
+    "revision": "2a24a8543299da438f46039d07384efa",
+    "url": "/portfoliopankaj/static/media/favicon1.2a24a854.ico"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/portfoliopankaj/static/media/feelingProud.a73310ea.svg"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/portfoliopankaj/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "/portfoliopankaj/static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/portfoliopankaj/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/portfoliopankaj/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/portfoliopankaj/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "66c987005ab0499d2801105d74f81d02",
+    "url": "/portfoliopankaj/static/media/google_logo.66c98700.jpg"
+  },
+  {
+    "revision": "6d325fec19fe3b6b169d9012f9a1dfb9",
+    "url": "/portfoliopankaj/static/media/ibm_logo.6d325fec.png"
+  },
+  {
+    "revision": "c350d7996596e9909fe72c0938cc9ee5",
+    "url": "/portfoliopankaj/static/media/iiitk_logo.c350d799.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/portfoliopankaj/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/portfoliopankaj/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "4ba886bafb9ff0a5b71b800c47f45d90",
+    "url": "/portfoliopankaj/static/media/legato_logo.4ba886ba.png"
+  },
+  {
+    "revision": "260d57586012b0ed1ae78accc0bf7083",
+    "url": "/portfoliopankaj/static/media/logo512.260d5758.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfoliopankaj/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "13078d3724fd881a36204970293c49d1",
+    "url": "/portfoliopankaj/static/media/michigan_logo.13078d37.png"
+  },
+  {
+    "revision": "bc865a89a1bcdc7e25f34a0536414345",
+    "url": "/portfoliopankaj/static/media/microsoft_logo.bc865a89.jpg"
+  },
+  {
+    "revision": "dc75df84304aa6cbb377e7c37039bd41",
+    "url": "/portfoliopankaj/static/media/moon.dc75df84.svg"
+  },
+  {
+    "revision": "ef8000fc3629a6efa4c01e75e8c5ae72",
+    "url": "/portfoliopankaj/static/media/mozilla_logo.ef8000fc.jpg"
+  },
+  {
+    "revision": "cd7077f7a08bffdb11e13093424e13fd",
+    "url": "/portfoliopankaj/static/media/nptel_logo.cd7077f7.png"
+  },
+  {
+    "revision": "6e7dc5ad476dd2d57600bca9521f3a5d",
+    "url": "/portfoliopankaj/static/media/portfolio.6e7dc5ad.gif"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfoliopankaj/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "f7964c6bf45e77547f1e5aae1e1dede0",
+    "url": "/portfoliopankaj/static/media/projects_image.f7964c6b.svg"
+  },
+  {
+    "revision": "bd5f4148a754968cb4444206539c565c",
+    "url": "/portfoliopankaj/static/media/pwa.bd5f4148.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfoliopankaj/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "da0e7c80b90df28dd3a590c80f3d4970",
+    "url": "/portfoliopankaj/static/media/stanford_logo.da0e7c80.png"
+  },
+  {
+    "revision": "4db24c0e662ca0fbacc25b3c959afedc",
+    "url": "/portfoliopankaj/static/media/sun.4db24c0e.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfoliopankaj/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/portfoliopankaj/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "871b519c05149abbe11b86d9386165ae",
+    "url": "/portfoliopankaj/static/media/tasin.871b519c.png"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/portfoliopankaj/static/media/ui_ux_design.67a6a11c.svg"
+  }
+]);
